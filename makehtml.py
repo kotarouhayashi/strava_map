50d453615b1df60001edf4425cba3b407c171ef3
hayashikotarou <884liberty@gmail.com>
2023-04-29 06:25:00 +0900

    all

--- makehtml.py
+++ makehtml.py
@@ -5,7 +5,6 @@ import gpxpy
 from folium.plugins import HeatMap
 
 # foliumを使用して、地図を作成 (初期表示は、緯度37.0、経度139.0、拡大率4倍)
-# map = folium.Map(location=[LATITUDE, LONGITUDE], zoom_start=ZOOM_LEVEL)
 map = folium.Map(location=[37, 139],zoom_start=4,tiles="Stamen Terrain", max_zoom=8)
 points = []
 
@@ -16,12 +15,9 @@ for gpx_file in sorted(glob.glob("data/activities/*.gpx")):
         for track in gpx.tracks:
           for segment in track.segments:
               for point in segment.points:
-                  # folium.PolyLine([point.latitude, point.longitude], color='cyan').add_to(map)
-                  # HeatMap([point.latitude, point.longitude], radius=8).add_to(map)
                   points.append([point.latitude, point.longitude])
 
 # GPXファイルからトレースデータを抽出し、地図上に表示
-            # folium.Marker([point.latitude, point.longitude], popup=point.time).add_to(map)
 folium.PolyLine(points, color='cyan').add_to(map)
 HeatMap(points, radius=8).add_to(map)
 
