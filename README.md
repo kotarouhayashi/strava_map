63c9ed077ed2f703e1b2d4a16f42db58a39cfc82
hayashikotarou <884liberty@gmail.com>
2023-04-29 06:08:52 +0900

    all

---
+++ README.md
@@ -0,0 +1,28 @@
+## Stravaの全データをPython,foliumを使ってHTMLで表示出力
+このプロジェクトでは、Stravaからエクスポートしたデータを取得し、Pythonのfoliumパッケージを使用して地図上に表示し、HTMLとして保存します。
+
+### 前提条件
+1. Stravaにアカウントを持っていること。
+1. Stravaからデータを一括エクスポートし、解凍しておくこと。
+   1.[データのエクスポートと一括エクスポート – Strava サポート](https://support.strava.com/hc/ja/articles/216918437-%E3%83%87%E3%83%BC%E3%82%BF%E3%81%AE%E3%82%A8%E3%82%AF%E3%82%B9%E3%83%9D%E3%83%BC%E3%83%88%E3%81%A8%E4%B8%80%E6%8B%AC%E3%82%A8%E3%82%AF%E3%82%B9%E3%83%9D%E3%83%BC%E3%83%88)
+2. Python 3.10がインストールされていること。（他は試していない。）
+
+### 手順
+1. ダウンロード
+```
+git clone git@github.com:kotarouhayashi/strava_map.git
+cd strava_map
+```
+
+2. ストラバからダウンロードしてきたデータの中のactivitieをstrava_map/data/以下に配置
+3. 仮想環境を構築
+```
+poetry add $( cat requirements.txt )
+poetry update
+poetry install
+```
+4. 仮想環境にてmakehtml.pyを実行
+```
+poetry run python makehtml.py
+```
+### アウトプット
---
+++ makehtml.py
@@ -0,0 +1,29 @@
+# パッケージのインポート
+import folium
+import glob
+import gpxpy
+from folium.plugins import HeatMap
+
+# foliumを使用して、地図を作成 (初期表示は、緯度37.0、経度139.0、拡大率4倍)
+# map = folium.Map(location=[LATITUDE, LONGITUDE], zoom_start=ZOOM_LEVEL)
+map = folium.Map(location=[37, 139],zoom_start=4,tiles="Stamen Terrain", max_zoom=8)
+points = []
+
+# StravaのGPXファイルを読み込み。
+for gpx_file in sorted(glob.glob("data/activities/*.gpx")):
+    with open(gpx_file, 'r') as f:
+        gpx = gpxpy.parse(f)
+        for track in gpx.tracks:
+          for segment in track.segments:
+              for point in segment.points:
+                  # folium.PolyLine([point.latitude, point.longitude], color='cyan').add_to(map)
+                  # HeatMap([point.latitude, point.longitude], radius=8).add_to(map)
+                  points.append([point.latitude, point.longitude])
+
+# GPXファイルからトレースデータを抽出し、地図上に表示
+            # folium.Marker([point.latitude, point.longitude], popup=point.time).add_to(map)
+folium.PolyLine(points, color='cyan').add_to(map)
+HeatMap(points, radius=8).add_to(map)
+
+# 地図をHTMLファイルとして保存
+map.save("map.html")
